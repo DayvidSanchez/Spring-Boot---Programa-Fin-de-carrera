2a0f982fbf8c8886f00382668bd434c8d742180a
Brayan Sanchez Navarro <55117302+DayvidSanchez@users.noreply.github.com>
2021-01-28 11:56:35 -0500

    Add files via upload

---
+++ src/test/java/pe/isil/ProyectoFinalDeCarreraApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalDeCarreraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
